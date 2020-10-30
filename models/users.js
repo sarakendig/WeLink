d49cd53c7e18bd678286a4dbebb88cc4e77d2cc2
sarakendig <sarakendig@gmail.com>
2020-10-30 15:42:03 -0400

    made users model

---
+++ models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
