625e37ee1f4161c2f3e5601be2a1d0e3456cd4c6
sarakendig <sarakendig@gmail.com>
2020-10-30 15:41:12 -0400

    made users controller

---
+++ controllers/users_controller.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt');
+const express = require('express');
+const users = express.Router();
+const User = require('../models/users.js');
+
+users.get('/new', (req, res) => {
+    res.render('users/new.ejs', {
+        currentUser: req.session.currentUser
+    })
+});
+
+users.post('/', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    User.create(req.body, (err, createdUser) => {
+        console.log('user is created', createdUser)
+        res.redirect('/')
+    });
+});
+
+module.exports = users;
