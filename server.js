593703b0a967dedf8210e30e28f5fe89a92f3923
sarakendig <sarakendig@gmail.com>
2020-10-30 14:31:01 -0400

    s

--- server.js
+++ server.js
@@ -5,9 +5,8 @@ const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
-const session = require('express-session');
 const db = mongoose.connection;
-const env = require('dotenv').config();
+
 //___________________
 //Port
 //___________________
@@ -44,11 +43,6 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
-
-//___________________
-//Session
-//___________________
-
 
 
 //___________________
