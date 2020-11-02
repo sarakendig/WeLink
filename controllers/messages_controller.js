1cd9097e7fb279596b7dc7367daaac088361a7bc
sarakendig <sarakendig@gmail.com>
2020-11-02 11:54:39 -0500

    index

--- controllers/messages_controller.js
+++ controllers/messages_controller.js
@@ -11,7 +11,7 @@ const isAuthenticated = (req, res, next) => {
 }
 
 // INDEX
-messages.get('/', (req, res) => {
+messages.get('/messages', (req, res) => {
     Message.find({}, (error, allMessages) => {
         res.render('messages/index.ejs', {
             messages: allMessages,
